44cc487ada0b9e859f407ffcdbdb3dde71687915
Katie McLaughlin <katie@glasnt.com>
2025-11-18 17:03:55 +1100

    generator functions get weird with side effects(?)

--- scripts/src/custard.ts
+++ scripts/src/custard.ts
@@ -201,7 +201,6 @@ export function matchPackages(config: Config, paths: string[], checkoutPath: str
 }
 
 export function* findPackages(config: Config, root: string): Generator<string> {
-  console.error(`Finding Packages in ${root}`)
   const excluded = asArray(config['exclude-packages']) || [];
   const files = fs.readdirSync(root, {withFileTypes: true});
   for (const file of files) {
